b943c431808aae8938f89cbdb5f11a3967b75aee
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-06-20 20:37:31 -0700

    stilling fixing db to work in postman with heroku

--- database/migrations/20200618150859_dataTables.js
+++ database/migrations/20200618150859_dataTables.js
@@ -2,19 +2,19 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('users', (tbl) => {
       tbl.increments();
-      tbl.string('name');
-      tbl.string('email').unique();
-      tbl.string('username', 50).notNullable().unique();
-      tbl.string('password', 50).notNullable();
+      tbl.varchar('name');
+      tbl.varchar('email').unique();
+      tbl.varchar('username', 50).notNullable().unique();
+      tbl.varchar('password', 50).notNullable();
     })
 
     .createTable('recipes', (tbl) => {
       tbl.increments();
-      tbl.string('title', 100).notNullable().index();
-      tbl.string('creator').index();
+      tbl.varchar('title', 100).notNullable().index();
+      tbl.varchar('creator').index();
       tbl.varchar('ingredients', 10000).notNullable();
       tbl.varchar('directions', 10000).notNullable();
-      tbl.string('category', 30).notNullable().index();
+      tbl.varchar('category', 30).notNullable().index();
       // foreign key
       tbl
         .integer('user_id')
